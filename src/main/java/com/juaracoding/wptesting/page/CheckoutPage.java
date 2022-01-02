42a259f76a9978922fc2146809354969fe186eae
FerdinPermana <94943661+FerdinPermana@users.noreply.github.com>
2022-01-02 11:11:22 +0700

    Update CheckoutPage.java

--- src/main/java/com/juaracoding/wptesting/page/CheckoutPage.java
+++ src/main/java/com/juaracoding/wptesting/page/CheckoutPage.java
@@ -1,9 +1,12 @@
 package com.juaracoding.wptesting.page;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.juaracoding.wptesting.driver.DriverSingleton;
 
@@ -16,10 +19,10 @@ private WebDriver driver;
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(css = ".button.wc-forward")
-	private WebElement btnViewcart;
+	@FindBy(xpath = "//*[@id=\"nav-menu-item-cart\"]/a/span/span[2]")
+	private WebElement btnCartPage;
 	
-	@FindBy(css = ".checkout-button.button.alt.wc-forward")
+	@FindBy(xpath = "//*[@id=\"post-6\"]/div/div/div[2]/div[2]/div/a")
 	private WebElement btnCheckout;
 	
 	@FindBy(id = "billing_first_name")
@@ -31,45 +34,70 @@ private WebDriver driver;
 	@FindBy(id = "billing_company")
 	private WebElement txtcompany;
 	
-//	@FindBy(css = "")
-//	private WebElement cmbcountry;
+	@FindBy(id = "select2-billing_country-container")
+	private WebElement selectCountry;
 	
 	@FindBy(id = "billing_address_1")
-	private WebElement txtaddress;
+	private WebElement txtaddress1;
+	
+	@FindBy(id = "billing_address_2")
+	private WebElement txtaddress2;
 	
 	@FindBy(id = "billing_city")
 	private WebElement txtcity;
 	
-//	@FindBy(css = "")
-//	private WebElement cmbprovince
+	@FindBy(id = "select2-billing_state-container")
+	private WebElement selectState;
+	
 	@FindBy(id = "billing_postcode")
 	private WebElement txtpostcode;
 	
 	@FindBy(id = "billing_phone")
 	private WebElement txtphone;
 	
-	@FindBy(id = "billing_email")
-	private WebElement txtemail;
+	@FindBy(id = "order_comments")
+	private WebElement txtAdditional;
 	
-	@FindBy(id = "place_order")
-	private WebElement btnplaceorder;
+	@FindBy(xpath = "//*[@id=\"terms\"]")
+	private WebElement checkbtnTerms;
 	
-	public void goToCheckout() {
-		btnViewcart.click();
+	@FindBy(xpath = "//*[@id=\"place_order\"]")
+	private WebElement btnCheckoutFinish;
+	
+	public void gotoCartPage() {
+		btnCartPage.click();
+	}
+	
+	public void gotoCheckout() {
 		btnCheckout.click();
 	}
 	
-	public void goToOrder(String firstname, String lastname, String company, String address, String city, String postcode, String phone, String email) {		
+	public void gotoFinish(String firstname, String lastname, String company, 
+			String address1, String address2, String city, String postcode, 
+			String phone, String additional, String country, String state) {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		js.executeScript("window.scrollBy(0,500)");
 		txtfirstname.sendKeys(firstname);
 		txtlastname.sendKeys(lastname);
 		txtcompany.sendKeys(company);
-//		cmbcity
-		txtaddress.sendKeys(address);
+		selectCountry.click();
+		new Select(selectCountry).selectByVisibleText(country);
+		txtaddress1.sendKeys(address1);
+		txtaddress2.sendKeys(address2);
+		js.executeScript("window.scrollBy(0,500)");
 		txtcity.sendKeys(city);
-//		cmbprovince
+		selectState.click();
+		new Select(selectState).selectByVisibleText(state);
 		txtpostcode.sendKeys(postcode);
 		txtphone.sendKeys(phone);
-		txtemail.sendKeys(email);
-		btnplaceorder.click();
+		js.executeScript("window.scrollBy(0,100)");
+		txtAdditional.sendKeys(additional);
+	}
+	
+	public void gotoFinish2() {
+		checkbtnTerms.click();
+		btnCheckoutFinish.click();
 	}
 }
